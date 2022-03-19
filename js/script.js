740f726e34325314afd4c4a241c96d18cd84eaf0
Joao Galhardi <joaogalhardi.dev@gmail.com>
2022-03-19 15:12:12 -0300

    otimizacao do menu mobile e outras alteracoes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// WhatsApp
+
 // Após 5 segundos passados do usuário na página será exibida a caixa de texto
 setTimeout(() => {
   document.getElementById("caixaTexto").style.visibility = 'visible'
@@ -7,3 +9,22 @@ setTimeout(() => {
 caixaTexto.onclick = () => {
   document.getElementById("caixaTexto").style.visibility = "hidden";
 };
+
+//Menu-Mobile
+const btnMobile = document.querySelector("#btn-mobile");
+const nav = document.querySelector("nav");
+const links = document.querySelectorAll(".header-menu a")
+
+function toggleMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
+  nav.classList.toggle("active");
+}
+
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    nav.classList.remove('active')
+  })
+})
+
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
